fe86a374121ccb02d23967c9ebd10189e183aad7
Itminan30 <itminan30@gmail.com>
2023-02-17 23:16:12 +0600

    getPin function added

--- script/app.js
+++ script/app.js
@@ -1,1 +1,10 @@
-console.log('app.js connected');
+function getPin(){
+    const pin = Math.round(Math.random()*10000);
+    const pinString = pin+"";
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        return getPin();
+    }
+}
